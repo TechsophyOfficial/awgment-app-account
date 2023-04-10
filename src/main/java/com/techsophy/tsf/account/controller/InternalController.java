d3fa6a4fa8629050a93a2f567d2fefea6284543a
rupaliT123 <rupali.t@techsophy.com>
2023-04-10 15:25:53 +0530

    dev: lint:0 coverage:85% userCreation: lint:0 coverage:85%

--- src/main/java/com/techsophy/tsf/account/controller/InternalController.java
+++ src/main/java/com/techsophy/tsf/account/controller/InternalController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import static com.techsophy.tsf.account.constants.AccountConstants.VERSION_V1;
+import static com.techsophy.tsf.account.constants.AccountConstants.*;
 
-@RequestMapping("/internal"+VERSION_V1+"/users/create/")
+@RequestMapping(INTERNAL+VERSION_V1+USER_CREATE)
 public interface InternalController {
     @PostMapping
     ApiResponse<InternalUserFormDataSchema> saveUser(@RequestParam("signature") String signature, @RequestBody @Validated InternalUserFormDataSchema userFormDataSchema, @RequestHeader HttpHeaders headers) throws JsonProcessingException;
